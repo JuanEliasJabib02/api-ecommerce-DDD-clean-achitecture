305d95141120ff9b21f2f5ddd6537855a1b9a384
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-04-27 00:35:44 -0500

    feat: cors, jsons, bodyparser

--- src/config/koa-config.ts
+++ src/config/koa-config.ts
@@ -1,11 +1,26 @@
 
 import Koa from 'koa';
-
-
+import cors from "@koa/cors";
+import bodyParser, { Options } from 'koa-bodyparser';
+import json from "koa-json";
 //Init app
 const app = new Koa()
 
 
 //Middlewares
+
+app.use(cors());
+app.use(json());
+
+interface CustomBodyParserOptions extends Options {
+  urlencoded?: boolean;
+}
+
+app.use(bodyParser({
+  urlencoded: true
+} as CustomBodyParserOptions));
+
+
+
 
 export default app;
